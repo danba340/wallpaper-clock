3db0f4209308a1c2398e2756ca393061a0dd70e3
danba340 <danba340@gmail.com>
2020-09-08 00:56:06 +0200

    Finished

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # wallpaper-clock
+
+## Run on startup with pm2
+
+```
+npm i -g pm2
+pm2 startup
+pm2 start index.js
+pm2 save
+```
---
+++ index.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+const path = require("path");
+const nodeHtmlToImage = require("node-html-to-image");
+const wallpaper = require("wallpaper");
+const cron = require("node-cron");
+const { v4: uuidv4 } = require("uuid");
+
+// Running every minute
+cron.schedule("* * * * *", () => {
+  // Read html file
+  const html = fs.readFileSync("./index.html", function (err, html) {
+    if (err) {
+      throw err;
+    }
+    return html;
+  });
+  // Generate a unique name for new wallpaper
+  const imgPath = `./wallpaperclock${uuidv4()}.png`;
+
+  // Create image from html file
+  nodeHtmlToImage({
+    output: imgPath,
+    html: html.toString("utf-8"),
+  }).then(() => {
+    console.log("The image was created successfully!");
+    //Remove last wallpaper image file if exists
+    wallpaper.get().then((oldFile) => {
+      if (oldFile.includes("wallpaperclock")) {
+        fs.unlink(oldFile, (err) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          console.log("Last image successfully removed");
+        });
+      }
+    });
+
+    // Set wallpaper with new image
+    wallpaper.set(imgPath).then((err) => {
+      console.log("Wallpaper set successfully");
+    });
+  });
+});
